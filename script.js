e880bdad565f2210e46f7203f5a5b2574c627c2e
glopera24 <glopera24@gmail.com>
2025-05-05 22:39:36 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-function calcularCuota() {
-  var monto = parseFloat(document.getElementById('monto').value);
-  var tiempo = parseFloat(document.getElementById('tiempo').value);
-  var interes = parseFloat(document.getElementById('interes').value) / 100 / 12;
-
-  var cuota = (monto * interes) / (1 - Math.pow(1 + interes, -tiempo));
-  document.getElementById('resultado').innerText = "Cuota mensual: $" + cuota.toFixed(2);
-}
